76857ea90438a724093d21b6ecee0bd612b89f6a
Cackumbabo <jeffchen223@gmail.com>
2020-11-27 13:32:51 -0800

    change

--- testscript.py
+++ testscript.py
@@ -4,6 +4,6 @@ from time import sleep
 camera = PiCamera()
 camera.start_preview()
 for i in range(5):
-    sleep(5)
+    sleep(0.5)
     camera.capture('/home/pi/Desktop/testimage%s.jpg' % i)
 camera.stop_preview()
