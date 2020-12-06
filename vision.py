df56628f8ea373520e5800ccd462b964c3e2c649
Cackumbabo <7357708+Cackumbabo@users.noreply.github.com>
2020-12-05 16:29:07 -0800

    Update vision.py

--- vision.py
+++ vision.py
@@ -16,10 +16,12 @@ def capture_board_state(camera):
     print("Finished capturing images")
     camera.stop_preview()
 
-def process_board_state(board, aruco_dict, parameters):
-    piecedict = ["WP", "WR", "WB", "WN", "WQ", "WK"]
-    if piece_dict == aruco.Dictionary_get(aruco.DICT_6X6_250):
-        piecedict = ["BQ", "BK", "BB", "BR", "BN", "BP"]
+def process_board_state(board):
+    parameters =  aruco.DetectorParameters_create() 
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    white_aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+    piecedict = ["BQ", "BK", "BB", "BR", "BN", "BP"]
+    white_piecedict = ["WP", "WR", "WB", "WN", "WQ", "WK"]
     board.reset()
     for k in range(5):
         frame = cv2.imread('/home/pi/Desktop/106a_project/board' + str(k) + '.jpg')
@@ -27,7 +29,14 @@ def process_board_state(board, aruco_dict, parameters):
         for i in range(len(ids)):
             c = corners[i][0]
             bby.insert_piece(board, c[:, 0].mean(), c[:, 1].mean(), piecedict[ids[i][0]])
-    print("finished passes")
+    print("finished black passes")
+    for k in range(5):
+        frame = cv2.imread('/home/pi/Desktop/106a_project/board' + str(k) + '.jpg')
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, white_aruco_dict, parameters=parameters)
+        for i in range(len(ids)):
+            c = corners[i][0]
+            bby.insert_piece(board, c[:, 0].mean(), c[:, 1].mean(), white_piecedict[ids[i][0]])
+    print("finished white passes")
     while True:
         board.retrieve()
         human_input = input("Correct? Type Y to confirm: ")
